dce8a108091e578dc48b7e725b9c891c8b39766b
mahahahajan <pulkit8.mahajan@gmail.com>
2021-02-09 17:52:44 -0600

    switichng to linux because this fucked my mac

--- Lab1/main.c
+++ Lab1/main.c
@@ -2,6 +2,7 @@
 #include <fcntl.h>
 #include <readline/readline.h>
 #include <signal.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -20,6 +21,7 @@ enum Status { EMPTY = -2,
 			  DONE = 1 };
 
 #define PROCESS_GROUP 10
+
 typedef struct process {
 	char* arg1;	  //Ex. this would be "ls", then argv should be "-l"
 	char** argv;  // all arguments
@@ -33,6 +35,7 @@ typedef struct process {
 	char* output_file;
 	char* input_file;
 	char* error_file;
+	int isBackgroundJob;
 	enum Side pipeSide;	 //TODO: create enum for sides
 } process;
 
@@ -40,27 +43,43 @@ typedef struct job {
 	int jobid;
 	char* jobStr;
 	pid_t pGid;
+	pid_t pGid2;
 	enum Status status;
 	struct job_t* next;
 } job;
 
+static void sig_handler(int signo);
+void printProcess(process* p);
+void cleanProcess(process* p);
+process* makeProcess(char* inputStr);
+process* splitProcess(process* currProcess);
+int runProcess(process* currProcess, int isPipe, int isRight, int fd[], pid_t pgid);
+job* getNextJob(job* jobsArr[]);
+char* enumToString(enum Status val);
+void update_jobs(job* jobsArr[], int mostRecentJob, int status);
+void clean_job(job* currJob);
+void print_jobs(job* jobsArr[], int mostRecentJob);
+
+int mostRecentJob = 0;
+int mostRecentFG = 0;
+job* jobsArr[20];
+
 //So apparently tokens is a keyword?
 static void sig_handler(int signo)
 {
-	int pid = getpid();
 	switch (signo) {
-	case SIGCHLD:
-		//TODO: This case
-		printf("Caught sig child \n");
-		break;
+	// case SIGCHLD:
+	// 	//TODO: This case
+	// 	printf("Caught sig child \n");
+	// 	break;
 	case SIGINT:
 		// printf("caught SIGINT, PID: %d \n", getpid());
-		kill(pid, SIGINT);
-		waitpid(pid, 0, WNOHANG | WUNTRACED);
+		kill(jobsArr[mostRecentJob]->pGid, SIGINT);
+		waitpid(most, 0, WNOHANG | WUNTRACED);
 		break;
 	case SIGTSTP:
 		// printf("caught SIGINT, PID: %d \n", getpid());
-		kill(pid, SIGTSTP);
+		kill(jobsArr[mostRecentJob]->pGid, SIGTSTP);
 		waitpid(pid, 0, WNOHANG | WUNTRACED);
 		break;
 	}
@@ -73,7 +92,6 @@ void printProcess(process* p)
 	for (int i = 0; i < (p->numTokens) + 1; i++) {
 		printf("curr arg is %s at index %i \n", p->argv[i], i);
 	}
-	// printf("")
 }
 void cleanProcess(process* p)
 {
@@ -89,6 +107,7 @@ void cleanProcess(process* p)
 	p->output_file = NULL;
 	p->input_file = NULL;
 	p->error_file = NULL;
+	p->isBackgroundJob = -1;
 }
 
 process* makeProcess(char* inputStr)
@@ -140,14 +159,25 @@ process* makeProcess(char* inputStr)
 		currToken = strtok(NULL, " ");
 	}
 	// tokensArr[numTokens] = NULL;
+
 	newProcess->argv[numTokens] = NULL;
 	newProcess->numTokens = numTokens;
 	newProcess->pipeLoc = pipeLoc;
 	newProcess->numRedirects = numRedirects;
 	if (pipeLoc == 0 && numRedirects == 0) {
 		newProcess->baseCommand = 1;
+
+		if (strcmp(newProcess->argv[numTokens - 1], "&") == 0) {
+			newProcess->isBackgroundJob = 1;
+			newProcess->argv[numTokens - 1] = NULL;
+		}
 	}
-	// printf("%s is in tokens (tokenize func) \n", tokensArr[numTokens - 1]);
+
+	// printf("MY first command is %s \n", newProcess->arg1);
+	// for (int i = 0; i < newProcess->numTokens; i++) {
+	// 	printf("token is %s \n", (newProcess->argv)[i]);
+
+	// }
 	return newProcess;
 }
 
@@ -198,6 +228,10 @@ process* splitProcess(process* currProcess)
 	rightProcess->numRedirects = numRedirects;
 	if (pipeLoc == 0 && numRedirects == 0) {
 		rightProcess->baseCommand = 1;
+	}
+	if (strcmp(rightProcess->argv[numTokens - 1], "&") == 0) {
+		rightProcess->isBackgroundJob = 1;
+		rightProcess->argv[numTokens - 1] = NULL;
 	}
 	// printf("%s is in tokens (tokenize func) \n", tokensArr[numTokens - 1]);
 	return rightProcess;
@@ -278,8 +312,7 @@ job* getNextJob(job* jobsArr[])
 
 	for (int i = 0; i < 20; i++) {
 		if (jobsArr[i]->status == EMPTY) {
-			//Found spot for job.
-
+			//Found spot for job
 			jobsArr[i]->status = RUNNING;
 			jobsArr[i]->jobid = i;
 			return jobsArr[i];
@@ -302,12 +335,56 @@ char* enumToString(enum Status val)
 	}
 }
 
-void print_jobs(job* jobsArr[])
+void printDoneJob(job* currJob)
 {
+	currJob->status = DONE;
+	printf("[%d]-   %s\t\t%s\n", currJob->jobid, enumToString(currJob->status), currJob->jobStr);
+	clean_job(currJob);
+}
+
+void update_jobs(job* jobsArr[], int mostRecentJob, int status)
+{
+	for (int i = 0; i < 20; i++) {
+		if (jobsArr[i]->status != EMPTY) {
+			int pid = jobsArr[i]->pGid;
+			int pid2 = -1;
+			int stat;
+			if (jobsArr[i]->pGid2 != -1) {
+				pid2 = jobsArr[i]->pGid2;
+				stat = waitpid(pid2, &status, WNOHANG | WUNTRACED);
+				if (stat != 0 && stat != -1) {
+					printDoneJob(jobsArr[i]);
+				}
+			}
+			else {
+				stat = waitpid(pid, &status, WNOHANG | WUNTRACED);
+				if (stat != 0 && stat != -1) {
+					printDoneJob(jobsArr[i]);
+				}
+			}
+		}
+	}
+}
+void clean_job(job* currJob)
+{
+	currJob->status = EMPTY;
+	currJob->pGid = -1;
+	currJob->pGid2 = -1;
+	currJob->jobid = -1;
+	currJob->jobStr = NULL;
+}
+
+void print_jobs(job* jobsArr[], int mostRecentJob)
+{
+	char use = '-';
 	for (int i = 0; i < 20; i++) {
 		// printf("jobs at %i is %s \n", i, enumToString(jobsArr[i].status));
 		if (jobsArr[i]->status != EMPTY) {
-			printf("[%d] - %s\t\t%s\n", i, enumToString(jobsArr[i]->status), jobsArr[i]->jobStr);
+			if (i == mostRecentJob) {
+				use = '+';
+			}
+			printf("[%d]%c %s\t\t%s\n", i, use, enumToString(jobsArr[i]->status), jobsArr[i]->jobStr);
+			use = '-';
 		}
 	}
 }
@@ -316,18 +393,28 @@ int main()
 {
 	char** myTokens = (char**)malloc(sizeof(char*) * 2000);
 	int fd[2];
-
+	int status;
 	// printf() displays the string inside quotation
-	job* jobsArr[20];
+	// signal(SIGCHLD, sig_handler);
+	signal(SIGTSTP, sig_handler);
+	signal(SIGINT, sig_handler);
+
 	for (int i = 0; i < 20; i++) {
 		jobsArr[i] = (job*)malloc(sizeof(job));
-		jobsArr[i]->status = EMPTY;
+		clean_job(jobsArr[i]);
 	}
+
 	while (1) {
+		// signal(SIGCHLD, sig_handler);
+		signal(SIGTSTP, sig_handler);
+
+		update_jobs(jobsArr, mostRecentJob, status);
+
 		char* input = readline("# ");
 		char* ogInput;
 		int pid = -1;
 		int pid2 = -1;
+		bool bgPresent = false;
 		// inputPtr = input;
 		if (strcmp(input, "") == 0 || strcmp(input, " ") == 0) {
 			// printf("Why am i not hitting this \n");
@@ -336,7 +423,6 @@ int main()
 		//double pointer like arraylist
 		if (input != NULL) {
 			//Want to save the string so that I can list in jobs
-			// printf(" the input is %s \n", input);
 			ogInput = strdup(input);
 		}
 
@@ -345,62 +431,55 @@ int main()
 		process* processLeft = NULL;
 		process* processRight = NULL;
 
-		int status;
 		//TODO: check if bg, fg, jobs, etc
 		if (strcmp(currProcess->arg1, "jobs") == 0) {
-			print_jobs(jobsArr);
+			print_jobs(jobsArr, mostRecentJob);
 			continue;
 		}
-		if(strcmp(currProcess->arg1, "bg") == 0){
-			
-		}
-		if (currProcess->baseCommand == 1) {  //No pipes, no redirects, run command as is
-			//Single command
-			// printProcess(currProcess);
-			// printf("\n \n \n");
-			// printf("test \n");
-			pid = runProcess(currProcess, 0, -1, NULL, -1);
-			waitpid(pid, &status, 0);
-			// printf("\n \n \n");
-			// printf("ran base command \n");
-			// printProcess(currProcess);
+		if (strcmp(currProcess->arg1, "bg") == 0) {
+			//TODO: Handle bg stuff
+			//So the way I understand it, bg finds the last stopped process and puts it in the background
+			printf("Most recent job was %i \n", mostRecentJob);
 			continue;
 		}
-		else {
-			if (currProcess->pipeLoc != 0) {
-				//TODO: set up pipes here
-				processLeft = currProcess;
-				//Split process turns one process with a pipe into 2
-				processRight = splitProcess(currProcess);
-				int index = currProcess->pipeLoc;
+		if (strcmp(currProcess->arg1, "fg") == 0) {
+			//TODO: Handle bg stuff
+			//So the way I understand it, bg finds the last stopped process and puts it in the background
+			printf("Most recent fg job was %i \n", mostRecentFG);
+			continue;
+		}
+
+		if (currProcess->pipeLoc != 0) {
+			//TODO: set up pipes here
+			processLeft = currProcess;
+			//Split process turns one process with a pipe into 2
+			processRight = splitProcess(currProcess);
+			int index = currProcess->pipeLoc;
+		}
+		if (currProcess->numRedirects != 0) {
+			//TODO: set up redirects
+			int outRedirectToken = currProcess->outRedirectLoc;
+			int inRedirectToken = currProcess->inRedirectLoc;
+			if (outRedirectToken > 0) {
+				currProcess->output_file = currProcess->argv[outRedirectToken];
+				currProcess->argv[outRedirectToken] = NULL;
+				// printf("the out file is %s \n", currProcess->output_file);
 			}
-			if (currProcess->numRedirects != 0) {
-				// printf("Have a redirect  \n");
-				//TODO: set up redirects
-				int outRedirectToken = currProcess->outRedirectLoc;
-				int inRedirectToken = currProcess->inRedirectLoc;
-				if (outRedirectToken > 0) {
-					currProcess->output_file = currProcess->argv[outRedirectToken];
-					currProcess->argv[outRedirectToken] = NULL;
-					// printf("the out file is %s \n", currProcess->output_file);
-				}
-				if (inRedirectToken > 0) {
-					currProcess->input_file = currProcess->argv[inRedirectToken];
-					currProcess->argv[inRedirectToken] = NULL;
-				}
+			if (inRedirectToken > 0) {
+				currProcess->input_file = currProcess->argv[inRedirectToken];
+				currProcess->argv[inRedirectToken] = NULL;
 			}
 		}
-		job* newJob = getNextJob(jobsArr);
-		// printf("Try and abed in the morning \n ");
-		newJob->jobStr = ogInput;
-		newJob->pGid = pid;
-		newJob->status = RUNNING;
-		newJob->next = NULL;
+
 		// newJob->jobid = getNextJob(jobsArr);
 		//TODO: check if bg job otherwise handle waits
-		// printf("Cool cool cool \n");
+
 		if (processRight != NULL) {	 //This means that i have a pipe for sure
-			pipe(fd);				 //TODO: handle error
+
+			if (processRight->isBackgroundJob == 1) {
+				bgPresent = true;
+			}
+			pipe(fd);  //TODO: handle error
 			pid = runProcess(processLeft, 1, 0, fd, -1);
 			// printf("PID1 is %i \n", pid);
 			// int pgid = getpgrp();
@@ -409,14 +488,46 @@ int main()
 			// waitpid(pid, &status, 0);
 		}
 		else {
-			// printf("testing \n");
+			if (currProcess->isBackgroundJob != -1) {
+				bgPresent = true;
+			}
 			pid = runProcess(currProcess, 0, -1, NULL, -1);
 		}
-		waitpid(pid, &status, 0);
+		job* newJob = getNextJob(jobsArr);
+		// printf("Try and abed in the morning \n ");
+		newJob->jobStr = ogInput;
+		newJob->pGid = pid;
+		newJob->status = RUNNING;
+		mostRecentJob = newJob->jobid;
+		newJob->next = NULL;
+
+		if (currProcess->isBackgroundJob == 1) {
+			waitpid(pid, &status, WNOHANG | WUNTRACED);
+		}
+		else {
+			mostRecentFG = newJob->jobid;
+			waitpid(pid, &status, WUNTRACED);
+			if (!WIFSTOPPED(status)) {
+				//TODO: Clean job;
+				// remove_job(job->jobid, (job_t*)root, NULL);
+				clean_job(jobsArr[mostRecentJob]);
+			}
+		}
 		if (pid2 != -1) {
+			newJob->pGid2 = pid2;
 			close(fd[0]);
 			close(fd[1]);
-			waitpid(pid2, &status, 0);
+			if (processRight->isBackgroundJob == 1) {
+				waitpid(pid2, &status, WNOHANG | WUNTRACED);
+			}
+			else {
+				waitpid(pid2, &status, WUNTRACED);
+				if (!WIFSTOPPED(status)) {
+					//TODO: Clean job;
+					// remove_job(job->jobid, (job_t*)root, NULL);
+					clean_job(jobsArr[mostRecentJob]);
+				}
+			}
 			free(processRight->argv);
 			free(processRight);
 		}
@@ -425,6 +536,7 @@ int main()
 		free(currProcess);
 	}
 	free(myTokens);
+	free(jobsArr);
 	return 0;
 }
 
